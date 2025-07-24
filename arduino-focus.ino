d826bdd6683b74cf282bea0f4b4a391cf53da0a6
eldanielhumberto <gamekik92@gmail.com>
2025-07-24 12:14:01 -0600

    feat: add break mode

--- arduino-focus.ino
+++ arduino-focus.ino
@@ -4,28 +4,79 @@
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
+// App configuration
+const int FOCUS_SESSION_MINUTES = 25;
+const int FOCUS_SESSION_SECONDS = 0;
+
+const int BREAK_SESSION_MINUTES = 5;
+const int BREAK_SESSION_SECONDS = 0;
+
 // Timer variables
-int focusMinutes = 25;
-int focusSeconds = 0;
+String titleText = "";
+int minutes = FOCUS_SESSION_MINUTES;
+int seconds = FOCUS_SESSION_SECONDS;
+
+enum Mode {
+  FOCUS,
+  BREAK,
+};
+
+Mode currentMode = FOCUS;
 
 void setup() {
   lcd.begin(16, 2);
-
-  lcd.print("Focus time");
+  changeMode(FOCUS);   
 }
 
 void loop() {
-  lcd.setCursor(0, 1);
+  timer();
 
-  delay(1000);
-  focusSeconds--;
-  if (focusSeconds < 0) {
-    focusMinutes--;
-    focusSeconds = 59;
+  if (minutes < 0 && seconds == 59) {
+    if (currentMode == FOCUS) {
+      changeMode(BREAK);
+    } else if (currentMode == BREAK) {
+      changeMode(FOCUS);
+    }
+  }
+}
+
+void timer() {
+  char timeText[6];
+  sprintf(timeText, "%02d:%02d", minutes, seconds);
+
+  lcd.setCursor(0, 1);
+  lcd.print(timeText);
+
+  seconds--;
+  if (seconds < 0) {
+    minutes--;
+    seconds = 59;
   }
 
-  char timeText[6];
-  sprintf(timeText, "%02d:%02d", focusMinutes, focusSeconds);
-  
-  lcd.print(timeText);
+  delay(1000);
+}
+
+void changeMode(Mode mode) {
+  switch (mode) {
+    case FOCUS:
+      titleText = "Focus mode";
+      minutes = FOCUS_SESSION_MINUTES;
+      seconds = FOCUS_SESSION_SECONDS;
+
+      currentMode = FOCUS;
+      break;
+    case BREAK:
+      titleText = "Break mode";
+      minutes = BREAK_SESSION_MINUTES;
+      seconds = BREAK_SESSION_SECONDS;
+
+      currentMode = BREAK;
+      break;
+    default:
+      titleText = "UNKNOWN";
+  }
+
+
+  lcd.setCursor(0, 0);
+  lcd.print(titleText);
 }
